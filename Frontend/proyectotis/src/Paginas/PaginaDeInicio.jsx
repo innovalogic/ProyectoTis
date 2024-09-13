df6fcb2ffed81076f7a2604a312d693ec6539217
Amadeo <amadeosilesdx2@gmail.com>
2024-09-13 19:18:53 -0400

    Iniciando Desarrollo Pagina de Inicio

---
+++ Frontend/proyectotis/src/Paginas/PaginaDeInicio.jsx
@@ -0,0 +1,11 @@
+// src/Paginas/PaginaDeInicio.jsx
+export default function PaginaDeInicio() {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600 mb-6">
+          ¡Bienvenido a la Página de Inicio!
+        </h1>
+        <p className="text-lg text-gray-700">Esta es la página principal de tu aplicación.</p>
+      </div>
+    );
+  }
