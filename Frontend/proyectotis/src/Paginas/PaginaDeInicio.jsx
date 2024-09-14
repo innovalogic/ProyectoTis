f43956edc1b2945e3e03a2406fed62bd021def9d
Amadeo <amadeosilesdx2@gmail.com>
2024-09-13 22:41:50 -0400

    Avance Pagina de Inicio

--- Frontend/proyectotis/src/Paginas/PaginaDeInicio.jsx
+++ Frontend/proyectotis/src/Paginas/PaginaDeInicio.jsx
@@ -1,11 +1,18 @@
-// src/Paginas/PaginaDeInicio.jsx
+import React from 'react';
+import NavbarInicioDeSesion from "../Componentes/NavBarInicioDeSesion";
+
+
 export default function PaginaDeInicio() {
     return (
-      <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
+      <div>
+        <NavbarInicioDeSesion />
+        <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
         <h1 className="text-4xl font-bold text-blue-600 mb-6">
           ¡Bienvenido a la Página de Inicio!
         </h1>
         <p className="text-lg text-gray-700">Esta es la página principal de tu aplicación.</p>
       </div>
+      </div>
+      
     );
   }
