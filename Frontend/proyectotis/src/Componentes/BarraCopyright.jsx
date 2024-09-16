da2c08d8a61bb3deb1ae3821d7cab908eee0c963
Amadeo <amadeosilesdx2@gmail.com>
2024-09-16 11:11:56 -0400

    Pagina de inicio diseño terminado

--- Frontend/proyectotis/src/Componentes/BarraCopyright.jsx
+++ Frontend/proyectotis/src/Componentes/BarraCopyright.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Copyright() {
     return (
-        <div className="bg-[#32569A] text-center p-2 w-full text-white">
+        <div className="bg-[#1E3664] text-center p-2 w-full text-white">
             <span className="text-sm">
                 Copyright © 2024 InnovaLogic - Aviso legal, política de privacidad y afiliación
             </span>
