4edf8add49f3f7ac2bcf7789979ab8b717fc2013
JhonnSteven23 <160976036+JhonnSteven23@users.noreply.github.com>
2024-09-17 07:58:37 -0400

    Implementacion de los archivos en App jsx para su acceso desde el local host

--- Frontend/proyectotis/src/Paginas/RegistroEmpresa.jsx
+++ Frontend/proyectotis/src/Paginas/RegistroEmpresa.jsx
@@ -6,48 +6,44 @@ export default function RegistroEmpresa() {
     return (
         <>
             <NavbarInicioDeSesion />
-            <div className="background-image">
+            <div className="background-color">
                 <div className="form-container">
-                    <h1 className="text-4xl font-bold italic" style={{ color: '#1F3765' ,margin:'15px'}}>Registro Estudiante</h1>
+                    <h1 className="text-4xl font-bold italic" style={{ color: '#1F3765' ,margin:'15px'}}>Registro Grupo Empresa</h1>
                     <form className="form-grid">
-                        <div className="form-row">
-                            <div className="input-group">
-                                <label htmlFor="nombre">Nombre</label>
-                                <input type="text" id="nombre" placeholder="Nombre" className="input-field" />
-                            </div>
-                            <div className="input-group">
-                                <label htmlFor="apellido">Apellido</label>
-                                <input type="text" id="apellido" placeholder="Apellido" className="input-field" />
-                            </div>
-                            <div className="input-group">
-                                <label htmlFor="codsiss">CodSISS</label>
-                                <input type="text" id="codsiss" placeholder="CodSISS" className="input-field" />
-                            </div>
+                    <div className="form-row">
+
+                        <div className="input-group">
+                            <label htmlFor="nombre">Nombre de Empresa</label>
+                            <input type="text" id="nombrEmpresa" placeholder="Nombre de Empresa" className="input-field" />
                         </div>
-                        <div className="form-row">
-                            <div className="input-group">
-                                <label htmlFor="codigoGrupo">Código de Grupo</label>
-                                <input type="text" id="codigoGrupo" placeholder="Código de Grupo" className="input-field" />
-                            </div>
-                            <div className="input-group">
-                                <label htmlFor="telefono">Número de Teléfono</label>
-                                <input type="tel" id="telefono" placeholder="Número de Teléfono" className="input-field" />
-                            </div>
-                            <div className="input-group">
-                                <label htmlFor="contrasena">Contraseña</label>
-                                <input type="password" id="contrasena" placeholder="Contraseña" className="input-field" />
-                            </div>
+                        <div className="input-group">
+                            <label htmlFor="apellido">Nombre corto de la Empresa</label>
+                            <input type="text" id="nombreCorto" placeholder="Nombre corto de la Empresa" className="input-field" />
                         </div>
-                        <div className="form-row">
-                            <div className="input-group">
-                                <label htmlFor="confirmarContrasena">Confirmar Contraseña</label>
-                                <input type="password" id="confirmarContrasena" placeholder="Confirmar Contraseña" className="input-field" />
-                            </div>
+                    </div>
+
+                    <div className="form-row">
+                        <div className="input-group">
+                            <label htmlFor="codsiss">Correo electronico de la empresa</label>
+                            <input type="text" id="correo" placeholder="Correo electronico de la empresa" className="input-field" />
                         </div>
-                        <div className="form-row button-row">
-                            <button type="button" className="btn-cancel">Cancelar</button>
-                            <button type="submit" className="btn-register">Registrar</button>
+                        <div className="input-group">
+                            <label htmlFor="codsiss">Nombre Representante Legal</label>
+                            <input type="text" id="nombreRep" placeholder="Nombre Representante Legal" className="input-field" />
                         </div>
+                    </div>    
+                    
+                    <div className="form-row">
+                        <div className="input-group">
+                            <label htmlFor="codsiss">Numero Representante Legal</label>
+                            <input type="text" id="numeroRep" placeholder="Numero Representante Legal" className="input-field" />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="codsiss">Nombre Representante Legal</label>
+                            <input type="text" id="codsiss" placeholder="CodSISS" className="input-field" />
+                        </div>
+                    </div> 
+                            
                     </form>
                 </div>
             </div>
