7e248db66d06f277e8b4e16b0f83f12ed9302aba
JhonnSteven23 <160976036+JhonnSteven23@users.noreply.github.com>
2024-09-22 21:13:28 -0400

    Conversion de la barra Lateral a componente para su uso general

--- Frontend/proyectotis/src/Paginas/RegistroEmpresa.jsx
+++ Frontend/proyectotis/src/Paginas/RegistroEmpresa.jsx
@@ -1,116 +1,22 @@
 import NavbarInicioDeSesion from "../Componentes/NavbarInicio";
-import styles from './RegistroEmpresa.module.css'; // Importa el CSS módulo
 import Copyright from '../Componentes/BarraCopyright';
+import BarraLateral from '../Componentes/BarraLateral';
 import { useForm } from 'react-hook-form'; 
-import { Sidebar, Menu, MenuItem,SubMenu } from 'react-pro-sidebar';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 export default function RegistroEmpresa() {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
-  const [collapsed, setCollapsed] = useState(false);
 
   return (
     <>
         <NavbarInicioDeSesion />
-
         <div style={{ display: 'flex', height: '100%', marginTop: '70px', backgroundColor: '#32569A' }}>
-          <Sidebar
-            collapsed={collapsed}
-            className="bg-[#32569A] text-white shadow-lg transition-all duration-300 ease-in-out"
-            style={{ width: collapsed ? '80px' : '250px', height: '100%' }} 
-          >
-            <div className={`${styles.sidebarHeader} bg-[#32569A]`}>
-              <div className="flex items-center justify-between p-4">
-                <h1 className={`${collapsed ? 'hidden' : 'block'} text-white font-bold text-2xl`}>Menú</h1>
-                <button
-                  onClick={() => setCollapsed(!collapsed)}
-                  className="bg-[#32569A] text-white p-2 rounded flex items-center justify-center"
-                >
-                  <img
-                    src="/src/Imagenes/Barra.png"
-                    alt={collapsed ? 'Expandir' : 'Colapsar'}
-                    className="w-6 h-6"
-                  />
-                </button>
-          </div>
-            <div className="text-center" style={{marginTop: '15px' }}>
-              <img src="/src/Imagenes/Estudiante.png" alt="Logo" className="w-16 h-16 inline-block" />
-            </div>
-        </div>
-        <h1 className={`${collapsed ? 'hidden' : 'block'} text-white font-bold text-2xl bg-[#32569A] text-center p-2`}>Estudiante</h1>
+          <BarraLateral/>
 
-    <Menu
-      menuItemStyles={{
-        button: {
-          backgroundColor: '#32569A',
-          color: 'white',
-          fontWeight: 'bold',
-          [`&.active`]: {
-            backgroundColor: '#25467C',
-          },
-        },
-      }}
-      className="bg-[#32569A]"
-    >
-      <MenuItem
-        className={`text-white font-bold ${styles.menuItem}`}
-        icon={<img src="/src/Imagenes/Inicio.png" alt="Inicio" className="w-8 h-8 inline-block" />}
-        component={<Link to="/documentation" />}
-      >
-        Inicio
-      </MenuItem>
-
-      <MenuItem
-        className={`text-white font-bold ${styles.menuItem}`}
-        icon={<img src="/src/Imagenes/Test.png" alt="Evaluaciones" className="w-8 h-8 inline-block" />}
-        component={<Link to="/calendar" />}
-      >
-        Evaluaciones
-      </MenuItem>
-
-      <MenuItem
-        className={`text-white font-bold ${styles.menuItem}`}
-        icon={<img src="/src/Imagenes/Calendar.png" alt="Calendario" className="w-8 h-8 inline-block" />}
-        component={<Link to="/e-commerce" />}
-      >
-        Calendario
-      </MenuItem>
-
-      <SubMenu
-        title="Empresa"
-        className="bg-[#32569A] text-white font-bold"
-        style={{ backgroundColor: '#32569A', color: 'white' }}
-        icon={<img src="/src/Imagenes/Grupo.png" alt="Empresa" className="w-8 h-8 inline-block" />}
-      >
-        <MenuItem className="text-white font-bold" component={<Link to="/empresa/overview" />}>
-          Registrar
-        </MenuItem>
-        <MenuItem className="text-white font-bold" component={<Link to="/empresa/departments" />}>
-          Planificacion
-        </MenuItem>
-        <MenuItem className="text-white font-bold" component={<Link to="/empresa/contact" />}>
-          Informacion
-        </MenuItem>
-      </SubMenu>
-      <div className="mt-28">
-    <MenuItem
-      className="text-white font-bold"
-      onClick={() => alert('Has cerrado sesión.')}
-      icon={<img src="/src/Imagenes/Logout.png" alt="Cerrar sesión" className="w-8 h-8 inline-block" />}
-    >
-      Cerrar sesión
-    </MenuItem>
-  </div>
-    </Menu>
-
-  </Sidebar>
-
-  <form onSubmit={handleSubmit(onSubmit)} className={`space-y-4 p-4 flex-1 bg-[#c2d2e9] rounded-md` }>
-  <h1 className="text-2xl font-bold text-[#32569A] text-center mb-4">Registro de Empresa</h1>
-  <div className="flex flex-col md:flex-row md:space-x-4">
-    <div className="flex flex-col w-full md:w-1/2">
+          <form onSubmit={handleSubmit(onSubmit)} className={`space-y-4 p-4 flex-1 bg-[#c2d2e9] rounded-md` }>
+            <h1 className="text-2xl font-bold text-[#32569A] text-center mb-4">Registro de Empresa</h1>
+            <div className="flex flex-col md:flex-row md:space-x-4">
+            <div className="flex flex-col w-full md:w-1/2">
       <label htmlFor="nombre" className="font-bold text-[#32569A]">
         Nombre de Empresa <span className="text-red-500">*</span>
       </label>
