e21c76b445652665817156ce9b3cdae364543fc1
Amadeo <amadeosilesdx2@gmail.com>
2024-09-23 15:05:11 -0400

    Resolviendo conflictos dev

--- Frontend/proyectotis/src/Paginas/PáginasEst/HomeEstudiante.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export const HomeEstudiante = () => {
-  return (
-    <div>
-      <p>HomeEstudiante</p>
-    </div>
-  )
-}
--- Frontend/proyectotis/src/Paginas/PáginasEst/PlanificacionGE.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export const PlanificacionGE = () => {
-    return (
-      <div className='Planificacion'>
-        <p>PlanificacionGE</p>
-      </div>
-    )
-  }
