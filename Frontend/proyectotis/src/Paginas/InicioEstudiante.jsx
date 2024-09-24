8f3fa7081bb619e5f92480cfd2fd71dcdea12286
EmanuelPeredo <emanuelvpc06@gmail.com>
2024-09-24 11:10:45 -0400

    Actualizacion navbar

--- Frontend/proyectotis/src/Paginas/InicioEstudiante.jsx
+++ Frontend/proyectotis/src/Paginas/InicioEstudiante.jsx
@@ -8,8 +8,10 @@ export default function InicioEstudiante() {
     return (
         <>
           <NavbarInicioDeSesion/>
-          <BarraLateral/>
-
+          <div>
+            <BarraLateral/>
+            <div></div>
+          </div>
           <BarraCopyright/>
         </>
     );
