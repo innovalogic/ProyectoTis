4cf0a13039525d3465a917bc44d5f4adfa4fc86c
Amadeo <amadeosilesdx2@gmail.com>
2024-09-27 15:38:09 -0400

    Respaldo de proyecto

--- Frontend/proyectotis/src/App.jsx
+++ Frontend/proyectotis/src/App.jsx
@@ -6,7 +6,6 @@ import InicioSesion from './Paginas/InicioSesion';
 import RegistroEstudiante from './Paginas/RegistroEstudiante';
 import RegistroEmpresa from './Paginas/RegistroEmpresa';
 import InicioEstudiante from './Paginas/InicioEstudiante';
-import RutasEst from './Paginas/RutasEst'
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Route path="RegistroEstudiante" element={<RegistroEstudiante/>}/>
         <Route path="RegistroEmpresa" element={<RegistroEmpresa/>}/>
         <Route path="InicioEstudiante" element={<InicioEstudiante/>}/>
-        <Route path="/*" element={<RutasEst/>}/>
       </Routes>
     </Router>
   );
