937b7d1256bdc3280d35f8c3d12151aa7b8e90f1
JhonnSteven23 <160976036+JhonnSteven23@users.noreply.github.com>
2024-10-28 22:20:00 -0400

    Inicio de trabajo en el area de notificaciones cargando el docente

--- Frontend/proyectotis/src/Paginas/InicioEstudiante.jsx
+++ Frontend/proyectotis/src/Paginas/InicioEstudiante.jsx
@@ -6,9 +6,14 @@ export default function InicioEstudiante() {
       return (
         <>
         <NavbarInicioDeSesion />
-          <div style={{ display: 'flex', height: '100%', marginTop: '70px' }}>
-            <BarraLateral/>
-          </div> 
+        <div style={{ display: 'flex', height: '100%', marginTop: '70px', backgroundColor: '#32569A' }}>
+          <BarraLateral/>
+            <form  className={`space-y-4 p-4 flex-1 bg-[#c2d2e9] rounded-md` }>
+            <div className="bg-[#32569A] text-white p-4 rounded-md flex items-center justify-center">
+        <span className="text-lg font-semibold">Nombre del Docente</span>
+      </div>
+          </form>
+        </div>
           <BarraCopyright/>
         </>
     );
