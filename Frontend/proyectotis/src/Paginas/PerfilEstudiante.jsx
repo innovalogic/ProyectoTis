7bc7bba582325f6d7b87b2a9891c49f455c53385
Amadeo <amadeosilesdx2@gmail.com>
2024-10-29 16:15:16 -0400

    Avance front

--- Frontend/proyectotis/src/Paginas/PerfilEstudiante.jsx
+++ Frontend/proyectotis/src/Paginas/PerfilEstudiante.jsx
@@ -11,7 +11,13 @@ export default function PerfilEstudiante() {
                 <div className="flex justify-center items-center w-full mt-[-100px]">
                     <div className="bg-[#1E3664] rounded-[75px] p-12 text-white w-[98%] md:w-[90%] lg:w-[80%] h-[70vh] shadow-2xl flex flex-col">
                         <h2 className="text-center font-bold text-3xl">PERFIL ESTUDIANTE</h2>
-                        {/* Contenido del recuadro aquí */}
+                        <div className="flex flex-col mt-6 ml-24 pt-8"> {/* Añadir margen superior aquí */}
+                            <p className="text-2xl font-bold">NOMBRE:</p>
+                            <p className="text-2xl font-bold">APELLIDO:</p>
+                            <p className="text-2xl font-bold">CODSIS:</p>
+                            <p className="text-2xl font-bold">TELEFONO:</p>
+                            <p className="text-2xl font-bold">CORREO:</p>
+                        </div>
                     </div>
                 </div>
             </div>
